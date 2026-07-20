a1f7cbd644c037220efabbd7abc37ee574014b0f
SinedKadis <sidbov@gmail.com>
2026-07-20 19:54:23 +0300

    CFE -> ECF rename

--- src/main/java/net/sinedkadis/terracompositio/ecf/DefaultECFHandler.java
+++ src/main/java/net/sinedkadis/terracompositio/ecf/DefaultECFHandler.java
@@ -165,13 +165,13 @@ public class DefaultECFHandler implements IECFHandler, INBTSerializable<Compound
     @Override
     public CompoundTag serializeNBT() {
         CompoundTag nbt = new CompoundTag();
-        nbt.putInt("CFE", this.getECF());
+        nbt.putInt("ECF", this.getECF());
         return nbt;
     }
 
     @Override
     public void deserializeNBT(CompoundTag tag) {
-        this.setECF(tag.getInt("CFE"));
+        this.setECF(tag.getInt("ECF"));
     }
 
     public int getMaxECF() {
