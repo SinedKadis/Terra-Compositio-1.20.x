bd37ff63f5717462b0b88dba631599205548cc98
SinedKadis <sidbov@gmail.com>
2026-07-21 18:44:05 +0300

    Fixed range for ECF Behaviour and scale tooltips

--- src/main/java/net/sinedkadis/terracompositio/block/behaviours/ECFHandlerBehaviour.java
+++ src/main/java/net/sinedkadis/terracompositio/block/behaviours/ECFHandlerBehaviour.java
@@ -93,7 +93,7 @@ public class ECFHandlerBehaviour implements IBEECFBehaviour, IHaveKnowledge {
 
 
     public int getRange(boolean inner) {
-        return member.getRange();
+        return member.getRange(inner);
     }
 
     @Override
@@ -229,7 +229,7 @@ public class ECFHandlerBehaviour implements IBEECFBehaviour, IHaveKnowledge {
                 TooltipHelper.addIfExist(TooltipHelper.Keys.MAX_ECF, t, data);
                 TooltipHelper.addIfExist(TooltipHelper.Keys.QUEUED, t, data);
             } else {
-                TooltipHelper.addScaleIfExist(TooltipHelper.Keys.ECF, TooltipHelper.Keys.MAX_ECF, tooltip, data);
+                TooltipHelper.addScaleIfExist(TooltipHelper.Keys.ECF, TooltipHelper.Keys.MAX_ECF, t, data);
             }
         });
 
